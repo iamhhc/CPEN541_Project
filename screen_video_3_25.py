cb04cd42d9acd99eb3f4444506f5fe1526ace321
zijun726911 <zijun112233@163.com>
2021-04-01 21:51:19 -0700

    若当前未检测到人脸则使用上一次检测的数据

--- screen_video_3_25.py
+++ screen_video_3_25.py
@@ -1,3 +1,4 @@
+import copy
 import time,threading
 from datetime import datetime
 from PIL import ImageGrab
@@ -12,7 +13,7 @@ face_classifier =cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_def
 window = tk.Tk()
 face0Data=[]
 face1Data=[]
-
+width, height = 1920,1080
 
 def saveHeadMovementData(face0Data :list, face1Data :list):
   print("in saveHeadMovementData")
@@ -45,6 +46,9 @@ def video_record():   # 录入视频
   global start_time
   start_time = time.time()
   i = 0
+  lastFace0=[-1,0,0,0,0]
+  lastFace1=[-1,0,0,0,0]
+
   while True:
     if flag:
       print("录制结束！")
@@ -61,65 +65,49 @@ def video_record():   # 录入视频
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # detecting_faces
     faces = face_classifier.detectMultiScale(gray, 1.5, 5)
-    if faces is ():
-      print('No faces detected!')
 
     timeStamp = time.time()
-    width, height = 1920,1080
 
+
+
+    face0 = lastFace0
+    face1 = lastFace1
     if len(faces)==0:
-        face0Data.append([timeStamp,0,0,0,0])
-        face1Data.append([timeStamp,0,0,0,0])
+        print('No faces detected!')
+
 
     elif len(faces)==1:
-      face=faces[0]
-      (x, y, w, h)=face
+      print('1 face detected!')
+
+      (x, y, w, h)=faces[0]
+
       if (x + (w / 2)) < width / 2: #face 0
-        face0Data.append([timeStamp,x, y, w, h])
-        face1Data.append([timeStamp,0,0,0,0])
+        face0=[timeStamp,x, y , w, h]
 
       else: #face 1
-        face0Data.append([timeStamp, 0, 0, 0, 0])
-        face1Data.append([timeStamp,x, y, w, h])
 
+        face1=[timeStamp,x, y, w, h]
 
-
-    else:  # 2 faces_found
+    elif len(faces)==2 :  # 2 faces_found
       for (x, y, w, h) in faces:
-        #if x < 1000 and y < 1000:
-        if (x + (w / 2)) < width / 2:
+        print('2 faces detected!')
+
+        if (x + (w / 2)) < width / 2: #屏幕左半边的脸识别为  faceid = 0
           faceid = 0
-          # draw_rectangle_around_face
-          face0Data.append([timeStamp,x, y, w, h])
-          cv2.rectangle(image, (x, y), (x + w, y + h), (127, 0, 255), 2)
-          #     cv2.imshow('faces',image)
-          #     cv2.waitKey(0)
-          print("face coord",str(i),",faceid:",str(faceid),":",x,y,x + w,y + h)
-          #faceid = faceid + 1
-          # cropping_face_only
-          roi_color = image[y:y + h, x:x + w]
-          roi_gray = gray[y:y + h, x:x + w]
-          # cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(255,255,0),2)
-        else:
+          face0=[timeStamp,x, y, w, h]
+          print("timeStamp",str(timeStamp),"faceid:",str(faceid),":",x,y,x + w,y + h)
+
+        else: #屏幕右半边的脸识别为  faceid = 1
           faceid = 1
-          # draw_rectangle_around_face
-          face1Data.append([timeStamp,x, y, w, h])
+          face1=[timeStamp,x, y, w, h]
+          print("timeStamp",str(timeStamp), ",faceid:", str(faceid), ":", x, y, x + w, y + h)
 
-          cv2.rectangle(image, (x, y), (x + w, y + h), (127, 0, 255), 2)
-          #     cv2.imshow('faces',image)
-          #     cv2.waitKey(0)
-          print("face coord", str(i), ",faceid:", str(faceid), ":", x, y, x + w, y + h)
-          #faceid = faceid + 1
-          # cropping_face_only
-          roi_color = image[y:y + h, x:x + w]
-          roi_gray = gray[y:y + h, x:x + w]
-          # cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(255,255,0),2)
-
+    face0Data.append(face0)
+    face1Data.append(face1)
+    lastFace0=face0
+    lastFace1=face1
     i = i + 1
-    #imm = cvtColor(np.array(im), COLOR_RGB2BGR) # 转为opencv的BGR模式
-    #video.write(imm)  #写入
-    # time.sleep(0.2) # 等待5秒再次循环
-    # m1.save('test_image.png')
+
 
 # GUI
 # window.wm_attributes('-topmost',1)
@@ -128,17 +116,7 @@ def on_press(key):   # 监听按键
   if key == keyboard.Key.home:
     flag = True # 改变
     return False # 返回False，键盘监听结束！
-def video_info():   # 视频信息
-  video = VideoCapture('%s.avi' % name)  # 记得文件名加格式不要错！
-  fps = video.get(CAP_PROP_FPS)
-  Count = video.get(CAP_PROP_FRAME_COUNT)
-  size = (int(video.get(CAP_PROP_FRAME_WIDTH)), int(video.get(CAP_PROP_FRAME_HEIGHT)))
-  print('帧率=%.1f'%fps)
-  print('帧数=%.1f'%Count)
-  print('分辨率',size)
-  print('视频时间=%.3f秒'%(int(Count)/fps))
-  print('录制时间=%.3f秒'%(final_time-start_time))
-  print('推荐帧率=%.2f'%(fps*((int(Count)/fps)/(final_time-start_time))))
+
 if __name__ == '__main__':
   flag = False
   th = threading.Thread(target=video_record)
@@ -146,6 +124,5 @@ if __name__ == '__main__':
   with keyboard.Listener(on_press=on_press) as listener:
     listener.join()
   time.sleep(1)  # 等待视频释放过后
-  # video_info()
 
 
